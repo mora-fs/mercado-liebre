95cef97c85e4c81e55bef01384a3b06cd7d22108
mora-fs <mora.fernandezstickar@gmail.com>
2021-06-13 02:01:47 -0300

    fin del módulo- todavía hay errores en los medias de los forms porque el uso de positions afectó al header

--- app.js
+++ app.js
@@ -11,8 +11,15 @@ app.listen(3000, ()=>{
 });
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, './views/header.html'));
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+
+app.get('/register', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
 /*
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'));
